c6638dd19294e24ec043048c7625f81a2356a996
Kaleb Barrett <ktbarrett.connectflour@gmail.com>
2017-01-15 22:51:32 -0500

    fixing merge conflict

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,6 @@
 
-<<<<<<< HEAD
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 	}
-=======
-public abstract class HelloWorld {
-	public abstract void JebBush();
->>>>>>> branch 'master' of https://github.com/Pitzmans1832/GP2000.git
 }
